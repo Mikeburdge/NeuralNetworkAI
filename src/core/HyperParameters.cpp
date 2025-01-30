6fbbfa55ddd36a54f7000b0b642b113ee319cd55
mburdge <Michael.burdge@hotmail.co.uk>
2025-01-30 16:11:41 +0000

    Revert "cleaned up hyperparameters"
    This reverts commit 3f5c5f5e4759a1b1d52a49f31bf0d5dc6c76431e.

--- src/core/HyperParameters.cpp
+++ src/core/HyperParameters.cpp
@@ -6,57 +6,69 @@
 
 int HyperParameters::defaultInputLayerSize = 784;
 int HyperParameters::defaultNumHiddenLayers = 2;
+
 int HyperParameters::defaultHiddenLayerSize = 256; // 128 / 256
 int HyperParameters::defaultOutputLayerSize = 10;
 
-float HyperParameters::defaultLearningRate = HyperParameters::learningRate = 0.001f;
-int HyperParameters::defaultBatchSize = HyperParameters::batchSize = 64;
-int HyperParameters::defaultEpochs = HyperParameters::epochs = 10;
+float HyperParameters::defaultLearningRate = 0.004f;
+int HyperParameters::defaultBatchSize = 64;
+int HyperParameters::defaultEpochs = 10000;
 
-double HyperParameters::defaultWeightDecay = HyperParameters::weightDecay = 0.0005;
-bool HyperParameters::defaultUseDropoutRate = HyperParameters::useDropoutRate = true;
-float HyperParameters::defaultDropoutRate = HyperParameters::dropoutRate = 0.2f;
-bool HyperParameters::defaultUseGradientClipping = HyperParameters::useGradientClipping = true;
-double HyperParameters::defaultGradientClipThreshold = HyperParameters::gradientClipThreshold = 1.0;
+double HyperParameters::defaultWeightDecay = 0.0000;
+bool HyperParameters::defaultUseDropoutRate = true;
+float HyperParameters::defaultDropoutRate = 0.3f;
+double HyperParameters::defaultGradientClipThreshold = 1.0;
+bool HyperParameters::defaultUseGradientClipping = true;
 
-CostType HyperParameters::cost = crossEntropy;
-ActivationType HyperParameters::activationType = ReLU;
+float HyperParameters::learningRate = defaultLearningRate;
+int HyperParameters::batchSize = defaultBatchSize;
+int HyperParameters::epochs = defaultEpochs;
+
+double HyperParameters::weightDecay = defaultWeightDecay;
+bool HyperParameters::useDropoutRate = defaultUseDropoutRate;
+float HyperParameters::dropoutRate = defaultDropoutRate;
+
+bool HyperParameters::useGradientClipping = defaultUseGradientClipping;
+double HyperParameters::gradientClipThreshold = defaultGradientClipThreshold;
+
+CostType HyperParameters::cost = cost_Count;
+ActivationType HyperParameters::activationType = Activation_Count;
 
 int HyperParameters::visualizationUpdateInterval = 5;
 
 void HyperParameters::ResetHyperParameters()
 {
-    learningRate = defaultLearningRate;
-    batchSize = defaultBatchSize;
-    epochs = defaultEpochs;
-
-    weightDecay = defaultWeightDecay;
-    useDropoutRate = defaultUseDropoutRate;
-    dropoutRate = defaultDropoutRate;
+	learningRate = defaultLearningRate;
+	batchSize = defaultBatchSize; 
+	epochs = defaultEpochs; 
+    
+	weightDecay = defaultWeightDecay;
+	useDropoutRate = defaultUseDropoutRate;
+	dropoutRate = defaultDropoutRate;
 }
 
 void HyperParameters::SetHyperParameters(HyperParameters hyperParameters)
 {
-    learningRate = hyperParameters.learningRate;
-    batchSize = hyperParameters.batchSize;
-    epochs = hyperParameters.epochs;
-    weightDecay = hyperParameters.weightDecay;
-    useDropoutRate = hyperParameters.useDropoutRate;
-    dropoutRate = hyperParameters.dropoutRate;
+	learningRate = hyperParameters.learningRate;
+	batchSize = hyperParameters.batchSize;
+	epochs = hyperParameters.epochs;
+	weightDecay = hyperParameters.weightDecay;
+	useDropoutRate = hyperParameters.useDropoutRate;
+	dropoutRate = hyperParameters.dropoutRate;
 
-    return;
+	return;
 }
 
 HyperParameters HyperParameters::GetHyperParameters()
 {
-    HyperParameters hyperParameters;
+	HyperParameters hyperParameters;
 
-    hyperParameters.learningRate = learningRate;
-    hyperParameters.batchSize = batchSize;
-    hyperParameters.epochs = epochs;
-    hyperParameters.weightDecay = weightDecay;
-    hyperParameters.useDropoutRate = useDropoutRate;
-    hyperParameters.dropoutRate = dropoutRate;
-
-    return hyperParameters;
+	hyperParameters.learningRate = learningRate;
+	hyperParameters.batchSize = batchSize;
+	hyperParameters.epochs = epochs;
+	hyperParameters.weightDecay = weightDecay;
+	hyperParameters.useDropoutRate = useDropoutRate;
+	hyperParameters.dropoutRate = dropoutRate;
+	
+	return hyperParameters;
 }
